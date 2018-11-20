491393485cedd541114aa3845c3d0e8404a8edff
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2018-11-20 15:23:46 +0100

    async pipe!

--- book-rating/src/app/book-details/book-details.component.ts
+++ book-rating/src/app/book-details/book-details.component.ts
@@ -3,6 +3,10 @@ import { ActivatedRoute } from '@angular/router';
 import { Book } from '../shared/book';
 import { BookStoreService } from '../shared/book-store.service';
 
+import { map, switchMap, retry } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+
 @Component({
   selector: 'br-book-details',
   templateUrl: './book-details.component.html',
@@ -11,18 +15,15 @@ import { BookStoreService } from '../shared/book-store.service';
 export class BookDetailsComponent implements OnInit {
 
   isbn: string;
-  book: Book; // TODO: implement me!
+  book: Observable<Book>;
 
   constructor(private route: ActivatedRoute, private service: BookStoreService) { }
 
   ngOnInit() {
-    this.route.paramMap.subscribe(
-      params => {
-        this.isbn = params.get('isbn');
-        this.service.getSingleBook(this.isbn)
-          .subscribe(book => this.book = book);
-      }
+    this.book = this.route.paramMap.pipe(
+      map(params => params.get('isbn')),
+      switchMap(isbn => this.service.getSingleBook(isbn)),
+      retry(3)
     );
   }
-
 }
